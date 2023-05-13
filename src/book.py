5c24e17812e31f015337626f7e5241bd4a045ec8
greenkaban <kochetkov@ispras.ru>
2023-05-13 21:13:12 +0300

    resolve

--- src/book.py
+++ src/book.py
@@ -2,8 +2,38 @@ import enum
 
 
 class Genre(enum.Enum):
-    pass
+    NOVEL = 'novel'
+    COMEDY = 'comedy'
 
 
 class Book:
-    pass
+    def __init__(self, price: float, title: str, genre: Genre):
+        if not (isinstance(price, float) & isinstance(title, str) & isinstance(genre, Genre)):
+            raise TypeError
+
+        if price < 0:
+            raise ValueError('Price should be positive')
+
+        self._price = price
+        self._title = title
+        self._genre = genre
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def genre(self) -> Genre:
+        return self._genre
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            raise ValueError(f'{other} is not Book')
+        return self._genre == other._genre and self._price == other._price and self._title == other._title
+
+    def __hash__(self):
+        return hash((self._title, self._price, self._genre))
--- src/cart.py
+++ src/cart.py
@@ -1,3 +1,25 @@
+from typing import List
+
+from src.book import Book
+
 
 class Cart:
-    pass
+    def __init__(self):
+        self._books: List[Book, ...] = []
+
+    def add_book(self, book: Book):
+        if not isinstance(book, Book):
+            raise TypeError(f'{book} is not Book')
+        self._books.append(book)
+
+    def delete_book(self, book: Book):
+        if not isinstance(book, Book):
+            raise TypeError(f'{book} is not Book')
+        if book in self._books:
+            self._books.remove(book)
+        else:
+            raise ValueError(f'{book} is not in cart')
+
+    @property
+    def books(self):
+        return self._books.copy()
